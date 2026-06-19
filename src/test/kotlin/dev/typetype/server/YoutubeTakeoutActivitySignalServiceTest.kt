0dfd59c25da3fb9d07b8ad60e7b36ef5171a9a36
Priveetee <brahim.benyoucefpro@proton.me>
2026-06-19 19:32:17 +0200

    test: cover localized takeout imports

--- src/test/kotlin/dev/typetype/server/YoutubeTakeoutActivitySignalServiceTest.kt
+++ src/test/kotlin/dev/typetype/server/YoutubeTakeoutActivitySignalServiceTest.kt
@@ -16,7 +16,14 @@ class YoutubeTakeoutActivitySignalServiceTest {
         val result = YoutubeTakeoutActivitySignalService.parse(zip)
         assertEquals(1, result.first.size)
         assertTrue(result.first.first().channelUrl.contains("youtube.com/channel/UC999"))
-        assertEquals(listOf("https://www.youtube.com/watch?v=like123"), result.second)
+        assertEquals(
+            listOf(
+                "https://www.youtube.com/watch?v=like123",
+                "https://www.youtube.com/watch?v=like456",
+                "https://www.youtube.com/watch?v=like789",
+            ),
+            result.second,
+        )
         Files.deleteIfExists(zip)
     }
 
@@ -28,6 +35,8 @@ class YoutubeTakeoutActivitySignalServiceTest {
                 <html><body>
                 Vous vous êtes abonné à <a href="https://www.youtube.com/channel/UC999">Channel Name</a><br>
                 Vous avez aimé <a href="https://www.youtube.com/watch?v=like123">A video</a><br>
+                A aimé <a href="https://www.youtube.com/watch?v=like456">Another video</a><br>
+                Beğendiniz <a href="https://www.youtube.com/watch?v=like789">Turkish video</a><br>
                 </body></html>
             """.trimIndent().toByteArray())
             out.closeEntry()
