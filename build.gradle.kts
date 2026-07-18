b08debc49e65ce7fe49929ac3a040716e280e3c7
Priveetee <brahim.benyoucefpro@proton.me>
2026-07-18 13:00:03 +0200

    chore: pin combined SABR extractor hotfix

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ dependencies {
     implementation("io.ktor:ktor-server-call-logging-jvm")
     implementation("io.ktor:ktor-server-rate-limit-jvm")
     implementation("ch.qos.logback:logback-classic:1.5.38")
-    implementation("com.github.Priveetee.PipePipeExtractor:extractor:e1290b0e4c5275cd0c63df6d82c71e0c59f1ce24")
+    implementation("com.github.Priveetee.PipePipeExtractor:extractor:10dca80fcf0f246a0d87bcc240d74d07521af02d")
     compileOnly("com.github.TeamNewPipe:nanojson:1d9e1aea9049fc9f85e68b43ba39fe7be1c1f751")
     implementation("org.json:json:20260522")
     implementation("com.squareup.okhttp3:okhttp:5.4.0")
