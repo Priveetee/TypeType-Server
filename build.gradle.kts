234f2ad0d427b2ea0bc8d36714cdb6ff2bd59763
Priveetee <brahim.benyoucefpro@proton.me>
2026-07-24 13:16:03 +0200

    fix: restart SABR sessions after player reload

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ dependencies {
     implementation("io.ktor:ktor-server-call-logging-jvm")
     implementation("io.ktor:ktor-server-rate-limit-jvm")
     implementation("ch.qos.logback:logback-classic:1.5.38")
-    implementation("com.github.Priveetee.PipePipeExtractor:extractor:5b915f9103435a200c650848dbaee3476ed1eb15")
+    implementation("com.github.Priveetee.PipePipeExtractor:extractor:bc89250a00b5ca44ea2eb4890e663050bceba25c")
     compileOnly("com.github.TeamNewPipe:nanojson:1d9e1aea9049fc9f85e68b43ba39fe7be1c1f751")
     implementation("org.json:json:20260522")
     implementation("com.squareup.okhttp3:okhttp:5.4.0")
